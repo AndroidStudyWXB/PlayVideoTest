8148492a0864a5d01ae864189b5737668fdae207
WXB506 <1922686887@qq.com>
2016-03-08 23:47:00 +0800

    video study

---
+++ app/src/main/java/com/study/playvideotest/MainActivity.java
@@ -0,0 +1,77 @@
+package com.study.playvideotest;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.os.Environment;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+import android.widget.VideoView;
+
+import java.io.File;
+
+public class MainActivity extends Activity implements View.OnClickListener{
+
+    private VideoView videoView;
+    private Button play;
+    private Button pause;
+    private Button stop;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        play = (Button) findViewById(R.id.play);
+        play.setOnClickListener(this);
+
+        pause = (Button) findViewById(R.id.pause);
+        pause.setOnClickListener(this);
+
+        stop = (Button) findViewById(R.id.stop);
+        stop.setOnClickListener(this);
+
+        videoView = (VideoView) findViewById(R.id.video_view);
+
+        //initVideoPath();
+    }
+
+    private void initVideoPath() {
+        try {
+            File file = new File(Environment.getExternalStorageDirectory(), "movie.3gp");
+            Toast.makeText(this, file.getPath().toString(), Toast.LENGTH_LONG).show();
+            videoView.setVideoPath(file.getPath());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.play:
+                if(!videoView.isPlaying()) {
+                    videoView.start();
+                }
+                break;
+            case R.id.pause:
+                if(videoView.isPlaying()) {
+                    videoView.pause();
+                }
+                break;
+            case R.id.stop:
+                if(videoView.isPlaying()) {
+                    videoView.resume();
+                }
+                break;
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if(videoView != null) {
+            videoView.suspend();
+        }
+    }
+}
